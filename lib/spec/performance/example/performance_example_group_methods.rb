fcad88ffced5dbee8b54636f014d13ff9ca08ca5
Bob Remeika <mmol@grockit.com>
2010-01-27 10:30:44 -0800

    Condensed the code in .perform

--- lib/spec/performance/example/performance_example_group_methods.rb
+++ lib/spec/performance/example/performance_example_group_methods.rb
@@ -3,14 +3,13 @@ require "spec/performance/example/performance_execution_builder"
 module Spec
   module Performance
     module Example
+
       module PerformanceExampleGroupMethods
         def perform(description, options = {}, backtrace = nil, &implementation)
           options = Spec::Performance::Configuration.configured_options.merge(options)
+
           builder = PerformanceExecutionBuilder.new(options, &implementation)
-
-          implementation_with_performance_loop = builder.performance_proc
-
-          example(description, options, backtrace, &implementation_with_performance_loop)
+          example(description, options, backtrace, &builder.performance_proc)
         end
       end
 
